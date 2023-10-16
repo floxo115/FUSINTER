a528dffe409b1b476b3574ce3b072ce62deda94b
floxo115 <flojak@gmx.net>
2023-10-16 17:34:44 +0200

    started with fusinter_v1 tests

---
+++ test_fusinter_v1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pytest
+
+from fusinter_v1 import FUSINTERDiscretizer
+
+
+class TestFusinterV1:
+    def test_init_with_correct_input(self):
+        data_x = np.random.rand(50)
+        data_y = np.random.randint(0, 5, 50)
+        fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+        assert np.all(fusinter.data_x == data_x)
+        assert np.all(fusinter.data_y == data_y)
+
+    def test_init_with_arrays_of_diff_len_should_throw(self):
+        data_x = np.random.rand(59)
+        data_y = np.random.randint(0, 5, 50)
+        with pytest.raises(ValueError):
+            fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+    def test_init_with_data_y_being_not_of_int_dtype(self):
+        data_x = np.random.rand(59)
+        data_y = np.random.rand(59)
+        with pytest.raises(ValueError):
+            fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+

25b7223482338ceef0e8ebb8822456259a469d8b
floxo115 <flojak@gmx.net>
2023-10-17 15:56:40 +0200

    worked on fusinter_v1 init splitts

--- fusinter_v1.py
+++ fusinter_v1.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 
@@ -46,4 +48,40 @@ class FUSINTERDiscretizer:
         """
         :return: a numpy array of initial split points as described in step 2 of the algorithm
         """
-        pass
+
+        def get_label_of_next_value(index: int) -> Tuple[int, int]:
+            """
+            returns the label of the next value from a array of value
+            :param index: starting index
+            :return: Tuple of label and end index
+            """
+            value = self.data_x[index]
+            label = self.data_y[index]
+            index += 1
+            while index < len(self.data_x):
+                if value != self.data_x[index]:
+                    break
+
+                if label != self.data_y[index]:
+                    label = -1
+
+                index += 1
+
+            return label, index
+
+        splits = []
+        labels = []
+        index = 0
+        # we process the first value to get its label
+        label, index = get_label_of_next_value(index)
+        labels.append(label)
+
+        while index < len(self.data_x):
+            # for each value we compare the label to the one before and add splits accordingly
+            label, index = get_label_of_next_value(index)
+            # if the label to the one before differs OR if the label is -1 we add a split
+            if label != labels[-1] or labels[-1] == -1:
+                splits.append(self.data_x[index - 1])
+                labels.append(label)
+
+        return tuple(splits), tuple(labels)

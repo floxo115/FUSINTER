2fd955cc24bc3abeb954e862c1ac5fbd872a560d
floxo115 <flojak@gmx.net>
2023-10-23 13:08:11 +0200

    worked on testing fixtures

--- test_fusinter_v1.py
+++ test_fusinter_v1.py
@@ -5,23 +5,32 @@ import datasets
 from fusinter_v1 import FUSINTERDiscretizer
 
 
-@pytest.fixture
+@pytest.fixture()
+def init_data():
+    return [
+        (
+            datasets.paper_dataset_x,
+            datasets.paper_dataset_y,
+        ),
+        (
+            np.array([-10, -10, -10, -9, -9, -8, -8, -8, -8, 3, 3, 2, 2], dtype=np.float64),
+            np.array([1, 1, 1, 1, 1, 1, 2, 2, 1, 3, 3, 3, 3], dtype=np.int32),
+        )]
+
+
+@pytest.fixture()
 def init_split_fixture():
     """
     :return: a list of tuples containing (values, labels, expected splits, expected labels)
     """
     return [
         (
-            datasets.paper_dataset_x,
-            datasets.paper_dataset_y,
             np.array([2, 3, 13, 14, 15, 16, 17, 18, 19, 20, 23, 37, 38, 39, 40], dtype=np.float64),
-            np.array([0, 1, 0, -1, 0, 1, 0, 1, -1, 1, 0, 1, -1, -1, 1, 0], dtype=np.int32)
+            np.array([0, 1, 0, -1, 0, 1, 0, 1, -1, 1, 0, 1, -1, -1, 1, 0], dtype=np.int32),
         ),
         (
-            np.array([-10, -10, -10, -9, -9, -8, -8, -8, -8, 3, 3, 2, 2], dtype=np.float64),
-            np.array([1, 1, 1, 1, 1, 1, 2, 2, 1, 3, 3, 3, 3], dtype=np.int32),
             np.array([-8., 2.], dtype=np.float64),
-            np.array([1, -1, 3], dtype=np.int32)
+            np.array([1, -1, 3], dtype=np.int32),
         )
     ]
 
@@ -48,8 +57,8 @@ class TestFusinterV1:
         with pytest.raises(ValueError):
             fusinter = FUSINTERDiscretizer(data_x, data_y)
 
-    def test_get_initial_intervals(self, init_split_fixture):
-        for (data_x, data_y, exp_splits, exp_labels) in init_split_fixture:
+    def test_get_initial_intervals(self, init_data, init_split_fixture):
+        for (data_x, data_y), (exp_splits, exp_labels) in zip(init_data, init_split_fixture):
             fusinter = FUSINTERDiscretizer(data_x, data_y)
             act_splits, act_labels = fusinter.get_initial_intervals()
             assert np.all(act_splits == exp_splits)

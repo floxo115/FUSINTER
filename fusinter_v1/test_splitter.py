b106fc588b1c330e7ec87966422ce976ff99641c
floxo115 <flojak@gmx.net>
2023-11-04 10:54:43 +0100

    removed old tests

--- fusinter_v1/test_splitter.py
+++ fusinter_v1/test_splitter.py
@@ -5,46 +5,7 @@ import datasets
 from .splitter import Splitter
 
 
-@pytest.fixture()
-def init_data():
-    """
-    :return: a list of initial values and labels for initializing FUSINTERDiscretizer
-    """
-    return [
-        (
-            datasets.paper_dataset_x,
-            datasets.paper_dataset_y,
-        ),
-        (
-            np.array([-10, -10, -10, -9, -9, -8, -8, -8, -8, 2, 2, 3, 3], dtype=np.float64),
-            np.array([0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 2, 2], dtype=np.int32),
-        )]
-
-
-@pytest.fixture()
-def init_split_fixture():
-    """
-    :return: a list of tuples containing ( expected splits, expected labels)
-    """
-    return [
-        (
-            np.array([2, 3, 13, 14, 15, 16, 17, 18, 19, 20, 23, 37, 38, 39, 40], dtype=np.float64),
-            np.array([0, 1, 0, -1, 0, 1, 0, 1, -1, 1, 0, 1, -1, -1, 1, 0], dtype=np.int32),
-        ),
-        (
-            np.array([-8., 2.], dtype=np.float64),
-            np.array([0, -1, 2], dtype=np.int32),
-        )
-    ]
-
-
 class TestSplitter:
-    def test_get_initial_intervals_to_replace(self, init_data, init_split_fixture):
-        for (data_x, data_y), (exp_splits, exp_labels) in zip(init_data, init_split_fixture):
-            splitter = Splitter(data_x, data_y)
-            act_splits, act_labels = splitter.apply()
-            assert np.all(act_splits == exp_splits)
-            assert np.all(act_labels == exp_labels)
 
     @pytest.mark.parametrize("data_x,data_y,exp_splits", [
         (
--- fusinter_v1/test_table_manager.py
+++ fusinter_v1/test_table_manager.py
@@ -5,75 +5,7 @@ import datasets
 from .table_manager import TableManager
 
 
-@pytest.fixture()
-def init_data():
-    """
-    :return: a list of initial values and labels for initializing FUSINTERDiscretizer
-    """
-    return [
-        (
-            datasets.paper_dataset_x,
-            datasets.paper_dataset_y,
-        ),
-        (
-            np.array([-10, -10, -10, -9, -9, -8, -8, -8, -8, 3, 3, 2, 2], dtype=np.float64),
-            np.array([0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 2, 2], dtype=np.int32),
-        )]
-
-
-@pytest.fixture()
-def init_split_fixture():
-    """
-    :return: a list of tuples containing ( expected splits, expected labels)
-    """
-    return [
-        (
-            np.array([2, 3, 13, 14, 15, 16, 17, 18, 19, 20, 23, 37, 38, 39, 40], dtype=np.float64),
-            np.array([0, 1, 0, -1, 0, 1, 0, 1, -1, 1, 0, 1, -1, -1, 1, 0], dtype=np.int32),
-        ),
-        (
-            np.array([-8., 2.], dtype=np.float64),
-            np.array([0, -1, 2], dtype=np.int32),
-        )
-    ]
-
-
-@pytest.fixture
-def init_tables():
-    """
-    :return: a list of tuples containing (splits, split_labels, T matrix)
-    """
-    return [
-        (
-            np.array([1, 0, 26, 2, 3, 0, 3, 0, 2, 0, 5, 0, 1, 2, 0, 2, 0, 1, 0, 1, 0, 3, 0, 3, 1, 2, 0, 27, 2, 1, 2, 0],
-                     dtype=int).reshape(2, 16)
-        ),
-        (
-            np.array([5, 2, 0, 0, 2, 0, 0, 0, 4], dtype=int).reshape(3, 3),
-        )
-    ]
-
-
 class TestTableManager:
-    @pytest.mark.skip()
-    def test_get_initial_tables_to_replace(self, init_data, init_split_fixture, init_tables):
-        for (data_x, data_y), (init_splits, _), (exp_table) in zip(init_data, init_split_fixture, init_tables):
-            tm = TableManager(data_x, data_y)
-            act_table = tm.create_table(init_splits)
-            assert np.all(act_table == exp_table)
-
-    @pytest.mark.skip()
-    def test_compress_table_to_replace(self):
-        input_table = np.array([2, 1, 0, 0, 2, 4, 0, 3, 0], dtype=int).reshape(3, 3)
-        expected_tables = [
-            np.array([3, 0, 2, 4, 3, 0], dtype=int).reshape(3, 2),
-            np.array([2, 1, 0, 6, 0, 3], dtype=int).reshape(3, 2),
-        ]
-
-        fusinter = TableManager(np.array([1, 2, 3]), np.array([1, 1, 1]))
-        for i, expected_table in enumerate(expected_tables):
-            assert np.all(fusinter.compress_table(input_table, i) == expected_table)
-
     @pytest.mark.parametrize("data_x, data_y, init_splits, expected", [
         [
             datasets.paper_dataset_x,

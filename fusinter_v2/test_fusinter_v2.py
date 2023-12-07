1fe072a7df73c4dca44f0398c338447d0674145c
floxo115 <flojak@gmx.net>
2023-12-07 13:10:18 +0100

    copied fusinter_v1 as starting poiont for fusinter_v2

---
+++ fusinter_v2/test_fusinter_v2.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pytest
+
+from .fusinter_v2 import FUSINTERDiscretizer, shannon_entropy, quadratic_entropy
+
+
+class TestFusinterV1:
+    def test_init_with_correct_input(self):
+        data_x = np.random.rand(50)
+        data_y = np.random.randint(0, 5, 50)
+        fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+        sort_idx = np.argsort(data_x)
+        assert np.all(fusinter.data_x == data_x[sort_idx])
+        assert np.all(fusinter.data_y == data_y[sort_idx])
+
+    def test_init_with_arrays_of_diff_len_should_throw(self):
+        data_x = np.random.rand(59)
+        data_y = np.random.randint(0, 5, 50)
+        with pytest.raises(ValueError):
+            fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+    def test_init_with_data_y_being_not_of_int_dtype(self):
+        data_x = np.random.rand(59)
+        data_y = np.random.rand(59)
+        with pytest.raises(ValueError):
+            fusinter = FUSINTERDiscretizer(data_x, data_y)
+
+
+@pytest.mark.parametrize("input_table, alpha, lam, expected", [
+    (
+            np.array([2, 1, 0, 0, 2, 4, 0, 3, 0], dtype=int).reshape(3, 3),
+            0.5, 0.2,
+            0.7954323401173173,
+    ),
+    (
+            np.array([8, 0, 2, 9, 1, 2], dtype=int).reshape(2, 3),
+            0.6, 0.9,
+            1.5388406493870612,
+    )
+])
+def test_shannon_entropy(input_table, alpha, lam, expected):
+    assert np.isclose(shannon_entropy(input_table, alpha=alpha, lam=lam), expected)
+
+
+@pytest.mark.parametrize("input_table, alpha, lam, expected", [
+    (
+            np.array([5, 0, 2, 3], dtype=int).reshape(2, 2),
+            0.4, 0.6,
+            0.4935060520341391,
+    ),
+])
+def test_quadratic_entropy(input_table, alpha, lam, expected):
+    assert np.isclose(quadratic_entropy(input_table, alpha=alpha, lam=lam), expected)

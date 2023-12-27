6f0c1c455931ef51722e76b2f9838083787ff1a7
floxo115 <flojak@gmx.net>
2023-12-27 14:01:20 +0100

    worked on benchmark functions

--- benchmark_v1.py
+++ benchmark_v1.py
@@ -1,10 +1,20 @@
-import datasets
-from datasets import paper_dataset_y, paper_dataset_x
-from fusinter_v1 import FUSINTERDiscretizer
+import numpy as np
 from timeit import timeit
 
-def f():
-    fusinter = FUSINTERDiscretizer(paper_dataset_x, paper_dataset_y)
+from datasets import paper_dataset_y, paper_dataset_x
+from fusinter_v1 import FUSINTERDiscretizer
+
+
+def f(dataset_increase_factor = 3):
+    data_x = np.copy(paper_dataset_x)
+    data_y = np.copy(paper_dataset_y)
+    data_x = data_x + 40 * np.arange(0,dataset_increase_factor).reshape(dataset_increase_factor,1)
+    data_x = data_x.ravel()
+    data_y = np.tile(data_y, dataset_increase_factor)
+    fusinter = FUSINTERDiscretizer(data_x, data_y)
     fusinter.apply()
 
-print(f"f took {timeit('f()', setup='from benchmark_v1 import f', number=10**3)/10**3} seconds to run")
+
+if __name__ == "__main__":
+    print("benchmark v1")
+    print(f"f took {timeit('f()', setup='from benchmark_v1 import f', number=10 ** 0) / 10 ** 0} seconds to run")
--- benchmark_v2.py
+++ benchmark_v2.py
@@ -1,10 +1,17 @@
-import datasets
+import numpy as np
 from datasets import paper_dataset_y, paper_dataset_x
 from fusinter_v2 import FUSINTERDiscretizer
 from timeit import timeit
 
-def f():
-    fusinter = FUSINTERDiscretizer(paper_dataset_x, paper_dataset_y)
+def f(dataset_increase_factor = 3):
+    data_x = np.copy(paper_dataset_x)
+    data_y = np.copy(paper_dataset_y)
+    data_x = data_x + 40 * np.arange(0,dataset_increase_factor).reshape(dataset_increase_factor,1)
+    data_x = data_x.ravel()
+    data_y = np.tile(data_y, dataset_increase_factor)
+    fusinter = FUSINTERDiscretizer(data_x, data_y)
     fusinter.apply()
 
-print(f"f took {timeit('f()', setup='from benchmark_v2 import f', number=10**3)/10**3} seconds to run")
+if __name__ == "__main__":
+    print("benchmark v2")
+    print(f"f took {timeit('f()', setup='from benchmark_v2 import f', number=10 ** 2) / 10 ** 2} seconds to run")

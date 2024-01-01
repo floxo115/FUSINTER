f0e4725d4dd8e6aa14280ee35d97b11f4a8807f0
floxo115 <flojak@gmx.net>
2024-01-01 15:16:54 +0100

    changed benchmark parameters

--- benchmark_v1.py
+++ benchmark_v1.py
@@ -5,7 +5,7 @@ from datasets import paper_dataset_y, paper_dataset_x
 from fusinter_v1 import FUSINTERDiscretizer
 
 
-def f(dataset_increase_factor = 3):
+def f(dataset_increase_factor = 10):
     data_x = np.copy(paper_dataset_x)
     data_y = np.copy(paper_dataset_y)
     data_x = data_x + 40 * np.arange(0,dataset_increase_factor).reshape(dataset_increase_factor,1)
@@ -17,4 +17,4 @@ def f(dataset_increase_factor = 3):
 
 if __name__ == "__main__":
     print("benchmark v1")
-    print(f"f took {timeit('f()', setup='from benchmark_v1 import f', number=10 ** 0) / 10 ** 0} seconds to run")
+    print(f"f took {timeit('f()', setup='from benchmark_v1 import f', number=10 ** 1) / 10 ** 1} seconds to run")
--- benchmark_v2.py
+++ benchmark_v2.py
@@ -3,7 +3,7 @@ from datasets import paper_dataset_y, paper_dataset_x
 from fusinter_v2 import FUSINTERDiscretizer
 from timeit import timeit
 
-def f(dataset_increase_factor = 3):
+def f(dataset_increase_factor = 10):
     data_x = np.copy(paper_dataset_x)
     data_y = np.copy(paper_dataset_y)
     data_x = data_x + 40 * np.arange(0,dataset_increase_factor).reshape(dataset_increase_factor,1)

8dc0d7366a9a091dccbf8ab00a2bf9365f15e4b3
floxo115 <flojak@gmx.net>
2024-01-04 15:32:04 +0100

    added init for version 3

---
+++ fusinter_v3/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by floxo on 12/28/23.
+//
+
+#include "paper_data.h"
+#include <iostream>
+
+#include "lib/library.h"
+int main(){
+    auto splitter = lib::Splitter(paper_data_x, paper_data_y);
+    auto splits = splitter.apply();
+
+    for (auto el: splits){
+        std::cout << el << ", ";
+    }
+
+    std::cout << std::endl;
+
+}

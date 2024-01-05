de19dbb5484f55f09878bfd05e62e9d68333f31f
floxo115 <flojak@gmx.net>
2024-01-05 08:30:01 +0100

    removed example test case

--- fusinter_v3/lib/test_splitter.cpp
+++ fusinter_v3/lib/test_splitter.cpp
@@ -4,14 +4,7 @@
 #include "errors.h"
 #include "Splitter.h"
 
-//TEST_CASE( "Factorials are computed", "[factorial]" ) {
-//    REQUIRE( factorial( 1) == 1 );
-//    REQUIRE( factorial( 2) == 2 );
-//    REQUIRE( factorial( 3) == 6 );
-//    REQUIRE( factorial(10) == 3'628'800 );
-//}
-
-TEST_CASE("Splitter with unsorted input"){
+TEST_CASE("Splitter with unsorted input") {
     auto data_y = paper_data_y;
     auto data_x = paper_data_x;
 
@@ -21,11 +14,10 @@ TEST_CASE("Splitter with unsorted input"){
     }
 }
 
-TEST_CASE("Splitter with non matching labels and data vectors"){
+TEST_CASE("Splitter with non matching labels and data vectors") {
     auto data_y = paper_data_y(Eigen::seq(0, 80));
     auto data_x = paper_data_x;
     SECTION("should throw NOT_MATCHING_DATA_SIZES") {
         REQUIRE_THROWS_AS(lib::Splitter(data_x, data_y), lib::NOT_MATCHING_DATA_SIZES);
     }
-
 }

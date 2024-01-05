90ed03c33bc94c8a44eb6cbd5aa77787606c12c5
floxo115 <flojak@gmx.net>
2024-01-05 10:37:05 +0100

    implemented tests for splitter

--- fusinter_v3/lib/test_splitter.cpp
+++ fusinter_v3/lib/test_splitter.cpp
@@ -1,8 +1,12 @@
+#include<tuple>
+
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include "../paper_data.h"
 #include "errors.h"
 #include "Splitter.h"
+#include "typedefs.h"
 
 TEST_CASE("Splitter with unsorted input") {
     auto data_y = paper_data_y;
@@ -20,4 +24,27 @@ TEST_CASE("Splitter with non matching labels and data vectors") {
     SECTION("should throw NOT_MATCHING_DATA_SIZES") {
         REQUIRE_THROWS_AS(lib::Splitter(data_x, data_y), lib::NOT_MATCHING_DATA_SIZES);
     }
+}
+
+struct splitter_test_data {
+    lib::data_vec data_x;
+    lib::label_vec data_y;
+    std::vector<float> expected;
+};
+
+TEST_CASE("Splitter with different valid inputs") {
+    auto data = GENERATE(
+            splitter_test_data(
+                    paper_data_x,
+                    paper_data_y,
+                    {2, 3, 13, 14, 15, 16, 17, 18, 19, 20, 23, 37, 38, 39, 40}
+            ),
+            splitter_test_data(
+                    lib::data_vec {{-10, -10, -10., -9., -9., -8., -8., -8., -8., 2., 2., 3., 3.}},
+                    lib::label_vec{{0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 2, 2}},
+                    {-8., 2.}
+            )
+    );
+    auto splitter = lib::Splitter(data.data_x, data.data_y);
+    REQUIRE(splitter.apply() == data.expected);
 }

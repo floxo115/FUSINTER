e632735496132824f2b8e29adc44b1f10048e50d
floxo115 <flojak@gmx.net>
2024-01-08 13:16:38 +0100

    put all tests in one file because of troubles with linker. can split again later when cause of problem is found.

--- fusinter_v3/lib/tests.cpp
+++ fusinter_v3/lib/tests.cpp
@@ -1,3 +1,4 @@
+#define CATCH_CONFIG_MAIN
 #include<tuple>
 
 #include <catch2/catch_test_macros.hpp>
@@ -6,6 +7,7 @@
 #include "../paper_data.h"
 #include "errors.h"
 #include "Splitter.h"
+#include "TableManager.h"
 #include "typedefs.h"
 
 TEST_CASE("Splitter with unsorted input") {
@@ -47,4 +49,22 @@ TEST_CASE("Splitter with different valid inputs") {
     );
     auto splitter = lib::Splitter(data.data_x, data.data_y);
     REQUIRE(splitter.apply() == data.expected);
+}
+
+TEST_CASE("TableManager with unsorted input") {
+    auto data_y = paper_data_y;
+    auto data_x = paper_data_x;
+
+    data_x[0] = data_x[70];
+    SECTION("should throw NOT_SORTED_ERROR") {
+        REQUIRE_THROWS_AS(lib::TableManager(data_x, data_y), lib::NOT_SORTED_ERROR);
+    }
+}
+TEST_CASE("TableManager with unequal sized input") {
+    auto data_y = lib::label_vec {{0,0,0,1,1,1,1,1,1,0,0,0,0}};
+    auto data_x = paper_data_x;
+
+    SECTION("should throw NOT_SORTED_ERROR") {
+        REQUIRE_THROWS_AS(lib::TableManager(data_x, data_y), lib::NOT_MATCHING_DATA_SIZES );
+    }
 }

12abf4e539304755cc85ebe304df012dcec4e7f4
floxo115 <flojak@gmx.net>
2024-01-08 16:03:27 +0100

    added test for compress_table

--- fusinter_v3/lib/tests.cpp
+++ fusinter_v3/lib/tests.cpp
@@ -82,7 +82,7 @@ struct table_manager_test_input {
     lib::table expected;
 };
 
-TEST_CASE("TableManager with valid inputs") {
+TEST_CASE("TableManager create_table with valid inputs") {
     auto data = GENERATE(
             table_manager_test_input(
                     paper_data_x,
@@ -92,8 +92,8 @@ TEST_CASE("TableManager with valid inputs") {
                                 2, 2, 1, 0, 2, 2, 0}}.reshaped(2, 16)
             ),
             table_manager_test_input(
-                    lib::data_vec {{-10, -10, -10, -9, -9, -8, -8, -8, -8, 2, 2, 3, 3}},
-                    lib::label_vec {{0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 2, 2}},
+                    lib::data_vec{{-10, -10, -10, -9, -9, -8, -8, -8, -8, 2, 2, 3, 3}},
+                    lib::label_vec{{0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 2, 2}},
                     {-8., 2.},
                     lib::table{{5, 0, 0, 2, 2, 0, 0, 0, 4}}.reshaped(3, 3)
             )
@@ -101,4 +101,19 @@ TEST_CASE("TableManager with valid inputs") {
 
     auto tm = lib::TableManager(data.data_x, data.data_y);
     REQUIRE(tm.create_table(data.input_splits) == data.expected);
+}
+
+TEST_CASE("TableManager compress_table") {
+    auto input_table = lib::table(3, 3);
+    input_table << 2,1,0,0,2,4,0,3,0;
+    std::cout << input_table << std::endl;
+    auto expected_tables = std::vector<lib::table>{
+            lib::table{{3, 2, 3, 0, 4, 0}}.reshaped(3, 2),
+            lib::table{{2, 0, 0, 1, 6, 3}}.reshaped(3, 2)
+    };
+
+    auto table = lib::TableManager::compress_table(input_table, 0);
+    REQUIRE(table == expected_tables[0]);
+    table = lib::TableManager::compress_table(input_table, 1);
+    REQUIRE(table == expected_tables[1]);
 }

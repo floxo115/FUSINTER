f6f46125a5bd24b31e1f3469f621b613de5560a0
floxo115 <flojak@gmx.net>
2024-01-17 16:18:34 +0100

    reformulated benchmark tests with covertype dataset and ran for v2 and v2_2

--- benchmark_v1.py
+++ benchmark_v1.py
@@ -1,15 +1,37 @@
 from timeit import timeit
 
 from fusinter_v1 import FUSINTERDiscretizer
+from datasets.paper_dataset import *
+
+paper_dataset_x = paper_dataset_x.reshape(-1, 1)
+
+import pandas as pd
+
+cov_df = pd.read_csv("datasets/covtype.data", header=None)
+cov_y = cov_df.pop(cov_df.shape[1] - 1).to_numpy()
+cov_x = cov_df.to_numpy()[:, 0:10] # all noncatecorical features
 
 
 def f(data_x, data_y):
-    fusinter = FUSINTERDiscretizer(data_x, data_y)
-    fusinter.apply()
+    for col_idx in range(data_x.shape[1]):
+        x = data_x[:, col_idx].ravel()
+        fusinter = FUSINTERDiscretizer(x, data_y)
+        fusinter.apply()
 
 
 if __name__ == "__main__":
+    n_runs = 1
     print("benchmark v1")
     print("paper dataset", end=": ")
-    setup = "from benchmark_v1 import f\nfrom datasets import paper_dataset_y, paper_dataset_x"
-    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=3) / 3, 4))
+
+    setup = """
+from benchmark_v1 import f, \
+paper_dataset_x, paper_dataset_y, \
+cov_x, cov_y
+"""
+
+    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4))
+    print("covertype features 0 to 9", end=": ")
+    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4))
+
+
--- benchmark_v2.py
+++ benchmark_v2.py
@@ -3,24 +3,35 @@ from timeit import timeit
 from fusinter_v2 import FUSINTERDiscretizer
 from datasets.paper_dataset import *
 
+paper_dataset_x = paper_dataset_x.reshape(-1, 1)
+
 import pandas as pd
 
 cov_df = pd.read_csv("datasets/covtype.data", header=None)
 cov_y = cov_df.pop(cov_df.shape[1] - 1).to_numpy()
-cov_x0 = cov_df.pop(0).to_numpy()
+cov_x = cov_df.to_numpy()[:, 0:10] # all noncatecorical features
 
 
 def f(data_x, data_y):
-    fusinter = FUSINTERDiscretizer(data_x, data_y)
-    fusinter.apply()
+    for col_idx in range(data_x.shape[1]):
+        x = data_x[:, col_idx].ravel()
+        fusinter = FUSINTERDiscretizer(x, data_y)
+        fusinter.apply()
 
 
 if __name__ == "__main__":
+    n_runs = 1
     print("benchmark v1")
     print("paper dataset", end=": ")
-    setup = "from benchmark_v2 import f, paper_dataset_x, paper_dataset_y, cov_x0, cov_y"
-    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=3) / 3, 4))
-    print("cov feature 0", end=": ")
-    print(round(timeit("f(cov_x0, cov_y)", setup=setup, number=1) / 1, 4))
+
+    setup = """
+from benchmark_v2 import f, \
+paper_dataset_x, paper_dataset_y, \
+cov_x, cov_y
+"""
+
+    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4))
+    print("covertype features 0 to 9", end=": ")
+    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4))
 
 

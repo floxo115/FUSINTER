b9698959e3b837c98024b2dcc241370508434b83
floxo115 <flojak@gmx.net>
2024-02-04 12:59:50 +0100

    worked on benchmarks

--- benchmark_v2.py
+++ benchmark_v2.py
@@ -11,6 +11,9 @@ cov_df = pd.read_csv("datasets/covtype.data", header=None)
 cov_y = cov_df.pop(cov_df.shape[1] - 1).to_numpy()
 cov_x = cov_df.to_numpy()[:, 0:10] # all noncatecorical features
 
+wave_df = pd.read_csv("datasets/waveform.data", header=None)
+wave_y = wave_df.pop(wave_df.shape[1] - 1).to_numpy()
+wave_x = wave_df.to_numpy() # all noncatecorical features
 
 def f(data_x, data_y):
     for col_idx in range(data_x.shape[1]):
@@ -21,17 +24,20 @@ def f(data_x, data_y):
 
 if __name__ == "__main__":
     n_runs = 1
-    print("benchmark v1")
-    print("paper dataset", end=": ")
 
     setup = """
 from benchmark_v2 import f, \
 paper_dataset_x, paper_dataset_y, \
-cov_x, cov_y
+cov_x, cov_y, \
+wave_x, wave_y
 """
 
-    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4))
+    print("benchmark v2")
+    print("paper dataset", end=": ")
+    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
+
     print("covertype features 0 to 9", end=": ")
-    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4))
+    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
 
-
+    print("wave all features", end=": ")
+    print(round(timeit("f(wave_x, wave_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
--- benchmark_v3.py
+++ benchmark_v3.py
@@ -1,7 +1,8 @@
 from timeit import timeit
 
-from fusinter_v2_2_new_interface import FUSINTERDiscretizer
+from FUSINTER_v3_pybind import FUSINTERDiscretizer
 from datasets.paper_dataset import *
+
 
 paper_dataset_x = paper_dataset_x.reshape(-1, 1)
 
@@ -25,20 +26,20 @@ def f(data_x, data_y):
 
 if __name__ == "__main__":
     setup = """
-from benchmark_v2_2 import f, \
+from benchmark_v3 import f, \
 paper_dataset_x, paper_dataset_y, \
 cov_x, cov_y, \
 wave_x, wave_y
 """
-    n_runs = 3
-    print("benchmark v2.2")
+    n_runs = 1
+    print("benchmark v3")
 
     print("paper dataset", end=": ")
-    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
+    print(round(timeit("f(paper_dataset_x, paper_dataset_y)", setup=setup, number=n_runs) / n_runs, 4))
 
     print("covertype features 0 to 9", end=": ")
-    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
+    print(round(timeit("f(cov_x, cov_y)", setup=setup, number=n_runs) / n_runs, 4))
 
     print("wave all features", end=": ")
-    print(round(timeit("f(wave_x, wave_y)", setup=setup, number=n_runs) / n_runs, 4), flush=True)
+    print(round(timeit("f(wave_x, wave_y)", setup=setup, number=n_runs) / n_runs, 4))
 

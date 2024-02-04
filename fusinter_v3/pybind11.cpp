438413690b3c344771dfe367de68e56065746da7
floxo115 <flojak@gmx.net>
2024-02-04 13:19:48 +0100

    set up pybind compilation

--- fusinter_v3/pybind11.cpp
+++ fusinter_v3/pybind11.cpp
@@ -1,20 +1,13 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <pybind11/stl.h>
 #include <Eigen/Dense>
 #include <iostream>
+#include "lib/library.h"
 
-int add(int i, int j) {
-    return i + j;
-}
-
-Eigen::Matrix<int, -1, -1> & test_np_to_eigen(Eigen::Matrix<int, -1, -1> &m){
-    std::cout << m << std::endl;
-    return m;
-}
-
-PYBIND11_MODULE(EXAMPLE, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
-
-    m.def("add", &add, "A function that adds two numbers");
-    m.def("test_np_to_eigen", &test_np_to_eigen, "test if I can convert np to eigen");
+PYBIND11_MODULE(FUSINTER_v3_pybind, m) {
+    pybind11::class_<lib::FUSINTERDiscretizer>(m, "FUSINTERDiscretizer")
+            .def(pybind11::init<float, float>())
+            .def("fit", &lib::FUSINTERDiscretizer::fit);
+            //.def("transform", &lib::FUSINTERDiscretizer::transform)
 }
